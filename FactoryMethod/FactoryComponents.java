5a2fcdff9c0eb8f8ad59c2a0f339527db7f309b6
ddalio <magd45801@gmail.com>
2025-05-05 23:00:30 -0300

    modificación de Components y UML general

--- FactoryMethod/FactoryComponents.java
+++ FactoryMethod/FactoryComponents.java
@@ -1,33 +1,64 @@
 /* Este codigo es solo para mostrar la estructura tipica en general, 
  * no se espera que compile o ejecute */
 
-//Interfaz del producto
-public interface Product{
-	void use();
+// Interfaz del producto
+public interface ProductIF {
+    void operation1();
+    void operation2();
 }
 
-//Interfaz del creador
-public interface Creator{
-	Product createProduct();
+// Producto concreto 1
+public class ConcreteProduct1 implements ProductIF {
+
+    @Override
+    public void operation1() {
+       //Desarrollo de operation1
+    }
+
+    //Más operaciones si se desea
 }
 
-//Producto concreto
-public class ConcreteProduct implements Product{
-	
-	@Override
-	public void use(){
-		//Implementración concreta	
-	}
+// Producto concreto 2
+public class ConcreteProduct2 implements ProductIF {
 
-	//Otros metodos si se desea
+    @Override
+    public void operation1() {
+        //Desarrollo de operation1
+    }
+
+    //Más operaciones si se desea
 }
 
-public class ConcreteCreator implements Creator {
-
-	@Override 
-	public Product createProduct(){
-		//Impletanción concreta
-	}
-	//Otros métodos si se desea
+// Interfaz del creador
+public interface FactoryIF {
+    ProductIF createProduct(String discriminator);
 }
+
+// Fábrica concreta
+public class Factory implements FactoryIF {
+
+	//discriminator sería el atributo que se utiliza para elegir entre los 
+	//concrete products que hay
+    @Override
+    public ProductIF createProduct(Object discriminator) {
+        //Elección de qué producto concreto se utilizaría
+    }
+}
+
+// Clase solicitante de creación
+public class CreationRequester {
+
+    private FactoryIF factory;
+
+    public CreationRequester(FactoryIF factory) {
+        this.factory = factory;
+    }
+
+    public ProductIF newProduct(boolean tipo) {
+        ProductIF product = factory.createProduct(tipo);
+
+        return product;
+    }
+}
+
 
